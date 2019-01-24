a82270245a1975c91c0ffdc78c5bc59b64b20e3f
hyungseok seo <hyungseokseo@hyungseokui-MacBook-Pro.local>
2019-01-24 12:45:59 +0900

    4-20 Creating the Feed Header

--- components/BackNavButton/index.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { TouchableWithoutFeedback, View, StyleSheet } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
--- routes/HomeRoute.js
+++ routes/HomeRoute.js
@@ -1,12 +1,30 @@
+import React from "react";
 import {createStackNavigator, createAppContainer} from "react-navigation";
 import FeedScreen from "../screens/FeedScreen";
 import sharedRoutes, { sharedOptions } from "./sharedRoutes";
+import { Image } from "react-native";
+import NavButton from "../components/NavButton";
 
 // 스크린 스택 놓는 순서가 중요하다 
 const HomeRoute = createStackNavigator(
   {
     Home: {
-      screen: FeedScreen
+      screen: FeedScreen,
+      navigationOptions: ({ navigation }) => ({
+        headerTitle: (
+          <Image
+            source={require("../assets/images/logo.png")}
+            style={{ height: 35 }}
+            resizeMode={"contain"}
+          />
+        ),
+        headerLeft: (
+          <NavButton
+            iconName={"ios-camera"}
+            onPress={() => navigation.navigate("TakePhoto")}
+          />
+        )
+      }),
     },
     ...sharedRoutes // 이걸 언팩이라고 하는데 sharedRoutes.js에 sharedRoutes 껄 다 가져오는 것같은
   },
